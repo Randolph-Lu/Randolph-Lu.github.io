0fa7977bdd305b0eb5634fdcf5af26d7c8a06c5e
Randolph-Lu <randolph_lu@163.com>
2023-09-17 15:04:10 +0800

    Deploy

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/randolphlu/",
+  base: "/",
 
   lang: "zh-CN",
   title: "",
